72ce7f2fc39b66749b05ddc7caa0174ee767d7b8
alysson912 <almenezes912@gmail.com>
2024-09-03 17:39:43 -0300

    start new project

---
+++ advancedProjectBootcamp/advancedProjectBootcamp/advancedProjectBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  advancedProjectBootcampApp.swift
+//  advancedProjectBootcamp
+//
+//  Created by Alysson Menezes on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct advancedProjectBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
